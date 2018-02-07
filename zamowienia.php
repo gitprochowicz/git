97cc1a73aac0b40f3a0e4ec1a82449880ef6ea88
User09 <User09@192.168.1.127>
2018-02-07 11:57:27 +0100

    fffff

--- zamowienia.php
+++ zamowienia.php
@@ -1,14 +1,31 @@
 <html>
     <head>
         <title>title</title>
+        <link href="bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
 
+        <div id="form_zam">
+            Imię <input type="text" name="asortyment" class="form-control"><br>
+            Nazwisko <input type="text" name="asortyment" class="form-control"><br>
+            Data zamówienia:<input type="date" name="data" class="form-control"><br>
+            Asortyment: <input type="text" name="asortyment" class="form-control"><br>
+            Ilość :<input type="text" name="ilosc" class="form-control"><br>
+            <center><button type="submit" class="btn btn-primary" >Dodaj zamówienie</button></center>
+            
+        </div>
+        
+        
         <?php
         
         
         
+        
+        
+        
         ?>
+        
         
         
         
